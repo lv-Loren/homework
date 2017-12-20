7ac2f582b949f69b3e4940878100c5519b6838a7
lvwei <lvwei@192.168.1.107>
2017-12-20 20:10:07 +0800

    lvwei

---
+++ src/main/java/lession171220/work1.java
@@ -0,0 +1,26 @@
+/**
+ * Project Name:dt59homework
+ * File Name:work1.java
+ * Package Name:lession171220
+ * Date:2017年12月20日下午8:09:42
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package lession171220;
+/**
+ * Description:   <br/>
+ * Date:     2017年12月20日 下午8:09:42 <br/>
+ * @author   lvwei
+ * @version
+ * @see
+ */
+public class work1 {
+
+    public static void main(String[] args) {
+
+        //  Auto-generated method stub
+
+    }
+
+}
+
