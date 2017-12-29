b8a615734f49f788873e29f2b3ee95d7520b78bf
lvwei <lvwei@lvwei-PC>
2017-12-29 19:47:11 +0800

    吕威

---
+++ src/main/java/lession1/lession171229/teacher1.java
@@ -0,0 +1,24 @@
+/**
+ * Project Name:dt59homework
+ * File Name:teacher.java
+ * Package Name:lession1.lession171229
+ * Date:2017年12月29日下午6:56:14
+ * Copyright (c) 2017, bluemobi All Rights Reserved.
+ */
+
+package lession1.lession171229;
+
+/**
+ * Description: <br/>
+ * Date: 2017年12月29日 下午6:56:14 <br/>
+ * 
+ * @author lvwei
+ * @version
+ * @see
+ */
+public class teacher1 extends work4 {
+    public void human() {
+        System.out.println("这个人的工作是教学");
+    }
+
+}
